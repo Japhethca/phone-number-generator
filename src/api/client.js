fc125bbb2c843418461af8e7475a7cbb5645c232
japhethca <chidiebere.anyigor@gmail.com>
2019-04-21 22:51:51 +0100

    update api URL

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:4000/api/v1/'
+  baseURL: process.env.API_URL || 'http://localhost:4000/api/v1/',
 });
 
 export default client;
